fd6279204352c5d2910acd557517540fe647d76b
macas5 <mkalinauskas1993@gmail.com>
2022-09-20 14:08:06 +0300

    add basic landing page structure

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,15 @@
+import Footer from '../../components/Footer/Footer';
+import MainBody from '../../components/MainBody/MainBody';
+import TopBar from '../../components/TopBar/TopBar';
+
+const MainPage = () => {
+  return (
+    <>
+      <TopBar />
+      <MainBody />
+      <Footer />
+    </>
+  );
+};
+
+export default MainPage;
