e67486c595c830c9aee1b15bb5a3be5b62cb4865
etitane <etitane28@gmail.com>
2022-09-21 10:00:33 +0300

    add footer structure and styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,41 @@
+import "./Footer.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footerWrapper">
+      <div className="footerSection">
+        <div className="footerSectionTitle">
+          <h3>Useful links</h3>
+        </div>
+        <div className="footerLinksWrapper">
+          <div className="footerLink">
+            <div>Home</div>
+          </div>
+          <div className="footerLink">
+            <div>E-Books</div>
+          </div>
+          <div className="footerLink">
+            <div>Most popular</div>
+          </div>
+        </div>
+      </div>
+      <div className="footerSection">
+        <div className="footerSectionTitle">
+          <h3>Contacts</h3>
+        </div>
+        <div className="footerContactsWrapper">
+          <div className="footerContact">Phone number: 12345678</div>
+          <div className="footerContact">Email: info@elibrary.com</div>
+          <div className="footerContact">Address: Elibrary 1234</div>
+        </div>
+      </div>
+      <div className="footerSection">
+        <div className="footerSectionTitle">
+          <h3>Logo</h3>
+        </div>        
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
