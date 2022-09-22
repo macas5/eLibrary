12f61fea56a8c4ccf2203f7236f2092cdbe35350
etitane <etitane28@gmail.com>
2022-09-22 11:04:23 +0300

    add login page structure and styles

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -2,7 +2,30 @@ import "./LoginForm.css";
 
 const LoginForm = () => {
   return (
-    <div>Login</div>      
+    <div className="loginFormWrapper">
+        <div className="loginFormTitle">Login</div>
+        <div className="loginFormBox">  
+            <div className="loginFormFirstSection">
+                <div className="loginFormRow">
+                    <div className="loginFormLabel">Username</div>
+                    <input type="text" />
+                </div>
+                <div className="loginFormRow">
+                    <div className="loginFormLabel">Password</div>
+                    <input type="password" />
+                </div>
+                <div className="loginFormButton">
+                    <button>Login</button>
+                </div>     
+            </div>   
+            <div className="loginFormSecondSection">
+                <div className="loginFormSectionTitle">You do not have an account?</div>
+                <div className="loginFormButton">
+                    <button>Register</button>
+                </div>   
+            </div>  
+        </div>
+    </div>      
   );
 };
 
