fa0bc951e61dce1ec240abd84ba4da0a4e424333
macas5 <mkalinauskas1993@gmail.com>
2022-09-22 15:36:05 +0300

    add basic settings layout

--- src/pages/Account/pages/Settings/Settings.jsx
+++ src/pages/Account/pages/Settings/Settings.jsx
@@ -1,3 +1,4 @@
+import { Button, TextField } from '@mui/material';
 import AccountInfo from '../../components/AccountInfo/AccountInfo';
 
 import './Settings.css';
@@ -6,6 +7,49 @@ const Settings = ({ user }) => {
   return (
     <div className="accountSettings">
       <AccountInfo user={user} />
+      <div className="accountSettingsWrapper">
+        <div className="basicInfo">
+          <h3>Basic info</h3>
+          <TextField
+            id="inputField"
+            label="Name"
+            variant="outlined"
+          />
+          <TextField
+            id="inputField"
+            label="email"
+            variant="outlined"
+          />
+        </div>
+        <div className="passwordField">
+          <h3>Password</h3>
+          <TextField
+            id="inputField"
+            label="Current Password"
+            type="password"
+            variant="outlined"
+          />
+          <TextField
+            id="inputField"
+            label="New Password"
+            type="password"
+            variant="outlined"
+          />
+          <TextField
+            id="inputField"
+            label="Repeat Password"
+            type="password"
+            variant="outlined"
+          />
+        </div>
+        <Button
+          id="submit"
+          variant="contained"
+          color="success"
+        >
+          Update
+        </Button>
+      </div>
     </div>
   );
 };
